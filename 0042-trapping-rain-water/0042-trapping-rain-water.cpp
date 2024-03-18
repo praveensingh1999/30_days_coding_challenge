85459b8178cb96a423089f3bdb7922e4e5a2f61d
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2024-03-18 15:06:37 +0530

    Time: 11 ms (54.82%), Space: 22.4 MB (57.13%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,38 +1,29 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left_max_height[height.size()];
-        int right_max_height[height.size()];
-        left_max_height[0]=height[0];
+        int pmax[height.size()];
+        pmax[0]=height[0];
         for(int i=1;i<height.size();i++)
         {
-            left_max_height[i]=max(height[i],left_max_height[i-1]);
+            pmax[i]=max(pmax[i-1],height[i]);
+            
         }
-        for(int i=0;i<height.size();i++)
-        {
-            cout<<left_max_height[i];
+        int smax[height.size()];
+        smax[height.size()-1]=height[height.size()-1];
+        for(int i=height.size()-2;i>=0;i--){
+            smax[i]=max(smax[i+1],height[i]);
         }
-        right_max_height[height.size()-1]=height[height.size()-1];
-        for(int j=height.size()-2;j>=0;j--)
-        {
-            right_max_height[j]=max(right_max_height[j+1],height[j]);
+        int ans=0;
+        for(int i=1;i<height.size()-1;i++){
+            int level = min(pmax[i-1],smax[i+1]);
+            int result = level-height[i];
+            if(result>0){
+                ans+=result;
+            }
         }
-        cout<<endl;
-        for(int i=0;i<height.size();i++)
-        {
-            cout<<right_max_height[i];
-        }
-        if(height.size()<=2)
-        {
-            return 0;
-        }
-        int amot=0;
-        for(int i=0;i<height.size();i++)
-        {
-            int t=min(left_max_height[i],right_max_height[i]);
-            amot+=(t-height[i]);
-        }
-        return amot;
+        return ans;
+        
         
     }
 };
